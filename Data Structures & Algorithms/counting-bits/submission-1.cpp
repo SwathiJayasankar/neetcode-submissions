33cd31b078d077a4b13bc9e5927300f8ce09fc12
SwathiJayasankar <swathi.jay02@gmail.com>
2026-08-11 09:38:50 +0530

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1,0);
+
+        for(int i=1; i<=n; i++){
+            int num = i;
+            int noOfOnes = 0;
+            
+            while(num > 0){
+                noOfOnes ++;
+                num = (num & num-1);
+            }
+            ans[i] = noOfOnes;
+        }
+        return ans;
+    }
+};
